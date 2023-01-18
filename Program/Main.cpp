76504aaf82e6b279b3f9f4af1c1a69e0a45970fb
sophie <bloxinas@list.ru>
2023-01-18 19:47:34 +0700

    алгоритм вынесен в отдельную функцию и работает с динамическими массивами

--- Program/Main.cpp
+++ Program/Main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 using namespace std;
-int main() {
-	string example[5] = {"hello", "world", "56", "t", ":-)"};
-	string result[5];
+const int MAX_LENGTH = 3;
+string* processArray(string origArray[], int size, int& newSize) {
+	string* current = new string[size];
 	int index = 0;
-	for (string current : example) {
-		if (current.length() <= 3) {
-			result[index++] = current;
+	for (int i = 0; i < size; i++) {
+		if (origArray[i].length() <= MAX_LENGTH) {
+			current[index++] = origArray[i];
 		}
 	}
-	for (string current : result) {
-		cout << current << " ";
+	string* result = new string[index];
+	for (int i = 0; i < index; i++) result[i] = current[i];
+	newSize = index;
+	return result;
+}
+int main() {
+	string example[5] = {"hello", "world", "56", "t", ":-)"};
+	int newSize;
+	string* result = processArray(example, 5, newSize);
+	for (int i = 0; i < newSize; i++) {
+		cout << result[i] << " " << i << " ";
 	}
 }
