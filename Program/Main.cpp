7d9ac0d6016900ad5d65375b037fae472375b986
sophie <bloxinas@list.ru>
2023-01-18 20:21:31 +0700

    начата работа над меню и реализована быстрая проверка

--- Program/Main.cpp
+++ Program/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 const int MAX_LENGTH = 3;
 string* processArray(string origArray[], int size, int& newSize) {
@@ -14,11 +15,48 @@ string* processArray(string origArray[], int size, int& newSize) {
 	newSize = index;
 	return result;
 }
-int main() {
-	string example[5] = {"hello", "world", "56", "t", ":-)"};
-	int newSize;
-	string* result = processArray(example, 5, newSize);
+void printArray(string example[], string result[], int size, int newSize) {
+	cout << "Изначальный массив: ";
+	for (int i = 0; i < size; i++) {
+		cout << example[i] << " ";
+	}
+	cout << "\nИзменённый массив: ";
 	for (int i = 0; i < newSize; i++) {
-		cout << result[i] << " " << i << " ";
+		cout << result[i] << " ";
+	}
+	cout << endl;
+}
+int main() {
+	setlocale(LC_ALL, "");
+	bool exit = 0;
+	while (exit == 0) {
+		system("cls");
+		cout << "Нажмите 1 для быстрой проверки;\nНажмите 2 для ввода с клавиатуры;\nНажмите 3 для выхода.\n";
+		int choose;
+		choose = _getch();
+		switch (choose - '0') {
+		case 1: {
+			system("cls");
+			string example1[4] = { "1234", "1567", "-2", "computer science" };
+			int newSize;
+			string* result = processArray(example1, 4, newSize);
+			printArray(example1, result, 4, newSize);
+			string example2[3] = { "Russia", "Denmark", "Kazan" };
+			result = processArray(example2, 3, newSize);
+			printArray(example2, result, 3, newSize);
+			string example3[4] = { "Hello", "2", "world", ":-)" };
+			result = processArray(example3, 4, newSize);
+			printArray(example3, result, 4, newSize);
+			system("pause");
+			break;
+		}
+		case 2: {
+			break;
+		}
+		case 3: {
+			exit = 1;
+			break;
+		}
+		}
 	}
 }
